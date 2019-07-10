01067895d6ec6a0df5337e0daa1d7d5f63c57140
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-10 16:33:00 -0700

    passing test for getting html

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('colors', () => {
+describe('color', () => {
   it('color test?', () => {
     return request(app)
       .get('/')
       .then(res => {
-        expect(res.text).toEqual(expect.stringContaining('list of colors'))
-      })
-  })
-})
+        expect(res.text).toEqual(expect.stringContaining('colors'))
+      });
+  });
+});
